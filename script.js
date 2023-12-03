28573eb26cfa9313bac522c395e7d3fc6ebed83b
Marko Matura <Marko.Matura@email.com>
2023-12-02 19:18:28 -0500

    Work on JS, add handleClick, checkWin, addMove, addMisses, checkAccuracy functionality

--- script.js
+++ script.js
@@ -11,6 +11,18 @@ const sportsArray = [soccerBall, soccerBall, baseball, baseball,
     tennisBall, tennisBall, football, football,
     basketBall, basketBall, golf, golf];
 
+const movesEl = document.querySelector('#moves-num');
+const missesEl = document.querySelector('#misses-num');
+const accuracyEl = document.querySelector('#accuracy-num')
+
+let randomlyPopulatedArray = []
+let selectedSquares = [];
+let matchedPairs = 0;
+let moves = 0;
+let misses = 0;
+let accuracy = 0;
+let gameWon = false;
+
 // The Fisher-Yates Shuffle Algorithm
 function shuffleArray(array) {
   for (let i = array.length - 1; i > 0; i--) {
@@ -20,7 +32,7 @@ function shuffleArray(array) {
   return array;
 }
 
-const randomlyPopulatedArray = shuffleArray([...sportsArray]);
+randomlyPopulatedArray = shuffleArray([...sportsArray]);
 
 
 squareEls.forEach(function (squareEl, index) {
@@ -30,6 +42,68 @@ squareEls.forEach(function (squareEl, index) {
 });
 
 function handleClick(index) {
-    squareEls[index].textContent = randomlyPopulatedArray[index];
+    if (selectedSquares.length < 2 && !selectedSquares.includes(index) && gameWon === false && !squareEls[index].textContent) {
+        selectedSquares.push(index);
+        squareEls[index].textContent = randomlyPopulatedArray[index];
+
+        if (selectedSquares.length === 2) {
+            const [index1, index2] = selectedSquares;
+            const content1 = randomlyPopulatedArray[index1];
+            const content2 = randomlyPopulatedArray[index2];
+            addMove();
+
+            if (content1 === content2) {
+                matchedPairs++;
+                console.log(matchedPairs);
+                // squareEls[index1].style.background = 'white';
+                // squareEls[index2].style.background = 'white';
+            } else {
+                setTimeout(() => {
+                    squareEls[index1].textContent = '';
+                    squareEls[index2].textContent = '';
+                }, 1000);
+                addMisses();
+            }
+
+            checkWin();
+            selectedSquares = [];
+        }
+    }
 }
 
+function checkWin() {
+    if (matchedPairs === 6) {
+        gameWon = true;
+        checkAccuracy();
+    } else {
+        gameWon = false;
+    }
+}
+
+function addMove() {
+    moves += 1;
+    movesEl.textContent = `${moves}`;
+}
+
+function addMisses() {
+    misses += 1;
+    missesEl.textContent = `${misses}`;
+}
+
+function checkAccuracy() {
+    accuracy = Math.round(((moves - misses) / moves)*100);
+    accuracyEl.textContent = `${accuracy}%`
+}
+
+
+// squaresMatch(content1, content2);
+// function squaresMatch(square1, square2) {
+//     if (square1 === square2) {
+//         matchedPairs++
+//     } else {
+//         setTimeout(() => {
+//             squareEls[index1].textContent = '';
+//             squareEls[index2].textContent = '';
+//         }, 1000);
+//     }
+// }
